7bc52e04d2dfdf41ce43b990a270aec2e71b4338
David Valdez <david.valdez210@gmail.com>
2015-09-12 01:08:08 -0500

    Add data

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,21 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../test_data.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/', function () use ($posts) {
+    $output = '';
+    foreach ($posts as $key => $post) {
+        $body = substr($post['body'],0, 50);
+        $output .= "<div>";
+        $output .= "<h1>{$post['title']}</h1>";
+        $output .= "<span>{$post['author']}</span>";
+        $output .= "<p>{$body}</p>";
+        $output .= "</div>";
+    }
+    return $output;
+});
+
 $app->run();
